927e7a0224515c329c9ae9b81ef1bd15fa627256
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2014-07-31 00:51:45 +0400

    for #8

--- arachnidium-core/src/main/java/org/arachnidium/core/aspect/AbstractAspect.java
+++ arachnidium-core/src/main/java/org/arachnidium/core/aspect/AbstractAspect.java
@@ -1,16 +1,35 @@
 package org.arachnidium.core.aspect;
 
+import java.lang.annotation.Annotation;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import org.aspectj.lang.JoinPoint;
 
 /**
  * This aspect is used by listeners
  */
 public abstract class AbstractAspect {
+
+	protected static enum WhenLaunch {
+		BEFORE(BeforeTargets.class), AFTER(AfterTargets.class);
+		/**
+		 * I use only @BeforeTarget and @AfterTarget here
+		 */
+		private final Class<? extends Annotation> annotation;
+		
+		private WhenLaunch(Class<? extends Annotation> annotation){
+			this.annotation = annotation;
+		}
+		
+		private Annotation[] getAnnotations(Method m){
+			return m.getAnnotationsByType(annotation);
+		}
+	}
 
 	/**
 	 * By this annotations methods of all listeners
@@ -28,7 +47,7 @@ public abstract class AbstractAspect {
 	
 	@Target(value = ElementType.METHOD)
 	@Retention(value = RetentionPolicy.RUNTIME)
-	protected @interface BeforeTargets {
+	private @interface BeforeTargets {
 		BeforeTarget[] value();
 	}	
 	
@@ -44,7 +63,7 @@ public abstract class AbstractAspect {
 	
 	@Target(value = ElementType.METHOD)
 	@Retention(value = RetentionPolicy.RUNTIME)
-	protected @interface AfterTargets {
+	private @interface AfterTargets {
 		AfterTarget[] value();
 	}
 	
@@ -61,7 +80,7 @@ public abstract class AbstractAspect {
 	
 	@Target(value = ElementType.PARAMETER)
 	@Retention(value = RetentionPolicy.RUNTIME) //Field marked by @SuppotField 
-	//can be inserted as parameter marked by
+	//can be inserted as a parameter marked by
 	protected @interface SupportParam {
 	}		
 	/**
@@ -69,7 +88,7 @@ public abstract class AbstractAspect {
 	 * *****************************************
 	 */
 	
-	public static void SupportField(Object valueForSupport, Object aspectObject){
+	public static void setSupportField(Object valueForSupport, Object aspectObject){
 		Field[] declaredFields = aspectObject.getClass().getDeclaredFields();
 		for (Field f: declaredFields){
 			if (!f.isAnnotationPresent(SupportField.class)){
@@ -85,6 +104,33 @@ public abstract class AbstractAspect {
 				throw new RuntimeException(e);
 			}
 		}
+	}
+	
+	protected static Object getSupportField(Object aspectObject, Class<?> requiredClass){
+		Field[] declaredFields = aspectObject.getClass().getDeclaredFields();
+		for (Field f: declaredFields){
+			if (!f.isAnnotationPresent(SupportField.class)){
+				continue;
+			}
+			
+			if (!f.getDeclaringClass().isAssignableFrom(requiredClass)){
+				continue;
+			}
+			try {
+				return f.get(aspectObject);
+			} catch (IllegalArgumentException | IllegalAccessException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Returns method of listener that should be executed before/after target method
+	 */
+	protected static Method getListenerMethod(JoinPoint joinPoint, Object aspectObject, WhenLaunch when){
+		//TODO Stub
+		return null;
 	}
 	
 }
