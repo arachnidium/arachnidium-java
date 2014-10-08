7eafc1e13a78abebe8ca31940344d093f44570ed
TikhomirovSergey <tichomirovsergey@gmail.com>
2014-10-08 21:43:02 +0400

    test improvement.

--- arachnidium-app-model/src/test/java/com/github/arachnidium/mobile/android/selendroid/testapp/ImplicitlyDefinedWebViewFrame.java
+++ arachnidium-app-model/src/test/java/com/github/arachnidium/mobile/android/selendroid/testapp/ImplicitlyDefinedWebViewFrame.java
@@ -2,6 +2,7 @@ package com.github.arachnidium.mobile.android.selendroid.testapp;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 
 import com.github.arachnidium.core.MobileScreen;
 import com.github.arachnidium.model.mobile.WebViewContent;
@@ -12,7 +13,7 @@ import com.github.arachnidium.model.support.annotations.classdeclaration.IfMobil
 
 @IfMobileDefaultContextIndex(index = 1)
 @IfMobileAndroidActivity(regExp = "WebViewActivity")
-@Frame(stringPath = "iframe1-name")
+@Frame(howToGet = How.ID, locator = "iframe1")
 public class ImplicitlyDefinedWebViewFrame extends WebViewContent {
 	
 	@FindBy(xpath = ".//*[@href='/foo']")
