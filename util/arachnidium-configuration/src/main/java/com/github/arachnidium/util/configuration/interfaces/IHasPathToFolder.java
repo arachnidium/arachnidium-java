3d68b280d9833ae48b7ce42e519524cc51ba2215
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2014-10-10 00:34:46 +0400

    javadocs Configuration

--- util/arachnidium-configuration/src/main/java/com/github/arachnidium/util/configuration/interfaces/IHasPathToFolder.java
+++
@@ -1,9 +0,0 @@
-package com.github.arachnidium.util.configuration.interfaces;
-
-/**
- * It is for all configurable/configuration entities 
- * which can return a path to some folder
- */
-public interface IHasPathToFolder {
-	public String getFolder();
-}
