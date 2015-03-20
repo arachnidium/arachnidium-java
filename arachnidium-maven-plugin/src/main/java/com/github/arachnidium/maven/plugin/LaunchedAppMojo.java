cac22e20c39344a0220c6b6d8187b941cee05331
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2015-03-21 02:25:50 +0400

    #31 structure improvement. pom configuration

---
+++ arachnidium-maven-plugin/src/main/java/com/github/arachnidium/maven/plugin/LaunchedAppMojo.java
@@ -0,0 +1,12 @@
+package com.github.arachnidium.maven.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+public class LaunchedAppMojo extends AbstractMojo{
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+    }
+}
