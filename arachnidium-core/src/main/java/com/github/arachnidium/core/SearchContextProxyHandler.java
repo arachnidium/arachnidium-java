7e92696836f734fb3626703e20d0c01211571d68
Sergey Tikhomirov <tichomirovsergey@gmail.com>
2015-06-14 19:31:35 +0300

    Fix of performance issue.
    It was been reproduced when ByAll and ByChained were used

--- arachnidium-core/src/main/java/com/github/arachnidium/core/SearchContextProxyHandler.java
+++ arachnidium-core/src/main/java/com/github/arachnidium/core/SearchContextProxyHandler.java
@@ -1,10 +1,14 @@
 package com.github.arachnidium.core;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
 
+import com.github.arachnidium.core.components.common.TimeOut;
 import org.openqa.selenium.By;
 import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.SearchContext;
@@ -26,16 +30,28 @@ class SearchContextProxyHandler implements InvocationHandler {
 		}
 
 	};
+
+    private void changeTimeOutAndRun(Runnable runnable){
+        TimeOut timeOut = handle.driverEncapsulation.getTimeOut();
+        long timeOutValue = timeOut.getImplicitlyWaitTimeOut();
+        TimeUnit timeUnit = timeOut.getImplicitlyWaitTimeUnit();
+        try {
+            timeOut.implicitlyWait(0, TimeUnit.SECONDS);
+            runnable.run();
+        } finally {
+            timeOut.implicitlyWait(timeOutValue, timeUnit);
+        }
+    }
 	
 	SearchContextProxyHandler(Handle handle){
 		this.handle = handle;
 	}
 
 	@Override
-	public Object invoke(Object o, Method m, Object[] args)
+	public Object invoke(final Object o, final Method m, Object[] args)
 			throws Throwable {
-		
-		WebDriver driver = handle.driverEncapsulation.getWrappedDriver();
+
+        final WebDriver driver = handle.driverEncapsulation.getWrappedDriver();
 		if (MethodInheritanceUtil.isOverriddenFrom(m, WrapsDriver.class))
 			return driver;
 		
@@ -45,26 +61,46 @@ class SearchContextProxyHandler implements InvocationHandler {
 		if (MethodInheritanceUtil.isOverriddenFromAny(m, 
 				classesThatRequireFocusOnTheHandle))
 			handle.switchToMe();		
-		
-		Object webDriverOrElement = null;
-		
-		if (MethodInheritanceUtil.isOverriddenFrom(m, WebDriver.class))
-			webDriverOrElement = driver;
-		
-		if (MethodInheritanceUtil.isOverriddenFrom(m, WebElement.class))
-			webDriverOrElement = driver.findElement(handle.by);
-		
+
+		if (MethodInheritanceUtil.isOverriddenFrom(m, WebElement.class)) {
+            final List<WebElement> elements = new ArrayList<>();
+            changeTimeOutAndRun(new Runnable() {
+                @Override
+                public void run() {
+                    elements.addAll(driver.findElements(handle.by));
+                }
+            });
+            if (elements.size() == 0)
+                throw new NoSuchElementException("There is no element that could be found using locator strategy " +
+                        handle.by.toString());
+
+            return m.invoke(elements.get(0), args);
+        }
+
+
 		if (MethodInheritanceUtil.isOverriddenFrom(m, 
 				SearchContext.class)){
-			if (o instanceof WebDriver)
-				return m.invoke(driver, new Object[] {(By) args[0]});
-			
-			return m.invoke(driver.findElement(handle.by), 
-					new Object[] {(By) args[0]});	
+            final List<Object> result = new ArrayList<>();
+            changeTimeOutAndRun(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        if (o instanceof WebDriver)
+                            result.add(m.invoke(driver, new Object[]{(By) args[0]}));
+                        if (o instanceof WebElement)
+                            result.add(m.invoke(driver.findElement(handle.by),
+                                    new Object[]{(By) args[0]}));
+                    }
+                    catch (InvocationTargetException|IllegalAccessException e){
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+            return result.get(0);
 		}
 		
-		if (webDriverOrElement != null)
-			return m.invoke(webDriverOrElement, args);
+		if (MethodInheritanceUtil.isOverriddenFrom(m, WebDriver.class))
+			return m.invoke(driver, args);
 		
 		return m.invoke(o, args);
 			
